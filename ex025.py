ea4ad08e250fbe4ebd712763fe4bf1a4ac87e5a3
Mallory Brewer <maloret@gmail.com>
2020-04-09 17:48:15 -0400

    complete drills through ex. 34

--- ex025.py
+++ ex025.py
@@ -12,6 +12,11 @@ def print_first_word(words):
 	word = words.pop(0)
 	print(word)
 
+def print_this_word(words):
+	"""No rhyme or reason to this one"""
+	word = words.pop(6)
+	print(word)
+
 def print_last_word(words):
 	"""Prints the last word after popping it off."""
 	word = words.pop(-1)
--- ex033.py
+++ ex033.py
@@ -1,16 +1,31 @@
-i = 0
-numbers = []
 
-while i < 6:
-	print("At the top i is %d" % i)
-	numbers.append(i)
+# set i's starting point at 0
+# save the numbers in list datatype
 
-	i = i + 1
-	print("Numbers now: ", numbers)
-	print("At the bottom i is %d" % i)
+def stop_before_then(x):
+	i = 0
+	numbers = []
 
+	# while i is less than 6, do the following logic
+	# logic is that while true, add a number incrementing by 1 until 5 is hit
+	while i < x:
+		print("At the top, i is %d" % i)
+		numbers.append(i)
+		print("")
 
-print("The numbers: ")
+	# this is the incrementer part of the loop 
+		i = i + 1
+		print("Numbers now: ", numbers)
+		print("At the bottom, i is %d" % i)
 
-for num in numbers:
-	print(num)
+	# print this string
+	print("The numbers: ")
+
+	# this is the instruction to iterate over the numbers list
+	# we are just using this for loop to print out a nice list at the bottom
+	for num in numbers:
+		print(num)
+
+stop_before_then(9)
+stop_before_then(100)
+stop_before_then(2)
