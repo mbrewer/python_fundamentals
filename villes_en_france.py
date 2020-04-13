82e360b7ccfb36eaa5b03327b111cdb34656b79e
Mallory Brewer <maloret@gmail.com>
2020-04-13 17:02:15 -0400

    set up a couple dictionaries with French data

---
+++ villes_en_france.py
@@ -0,0 +1,23 @@
+combien_de_départments = {
+	'Auvergne-Rhônes-Alpes': '12'
+	'Île-de-France': '8'
+	'Normandie': '5'
+	'Provence-Alpes-Côte d\'Azur': '8'
+	'Nouvelle-Aquitaine': '12'
+	'Grand Est': '10'
+	'Occitanie': '13'
+	'Bretagne': '4'
+}
+
+villes_et_régions = {
+	'Lyon': 'Auvergne-Rhônes-Alpes'
+	'Paris': 'Île-de-France'
+	'Caen': 'Normandie'
+	'Marseille': 'Provence-Alpes-Côte d\'Azur'
+	'Le Mont-Saint-Michel': 'Normandie'
+	'Grenoble': 'Auvergne-Rhônes-Alpes'
+	'Bordeaux': 'Nouvelle-Aquitaine'
+	'Strasbourg': 'Grand Est'
+	'Perpignan': 'Occitanie'
+	'Saint-Malo': 'Bretagne'
+}
