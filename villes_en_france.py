ea9b368f893d91f768eeafdcb0f66271c7e792db
Mallory Brewer <maloret@gmail.com>
2020-04-14 09:32:36 -0400

    fix syntax errors and encoding issue with French chars

--- villes_en_france.py
+++ villes_en_france.py
@@ -1,23 +1,26 @@
-combien_de_départments = {
-	'Auvergne-Rhônes-Alpes': '12'
-	'Île-de-France': '8'
-	'Normandie': '5'
-	'Provence-Alpes-Côte d\'Azur': '8'
-	'Nouvelle-Aquitaine': '12'
-	'Grand Est': '10'
-	'Occitanie': '13'
-	'Bretagne': '4'
+#!/usr/bin/env python
+# -*- coding: latin-1 -*-
+
+combien_de_departements = {
+                     'Auvergne-Rhônes-Alpes': 12,
+                     'Île-de-France': 8,
+                     'Normandie': 5,
+                     'Provence-Alpes-Côte d\'Azur': 8,
+                     'Nouvelle-Aquitaine': 12,
+                     'Grand Est': 10,
+                     'Occitanie': 13,
+                     'Bretagne': 4
 }
 
-villes_et_régions = {
-	'Lyon': 'Auvergne-Rhônes-Alpes'
-	'Paris': 'Île-de-France'
-	'Caen': 'Normandie'
-	'Marseille': 'Provence-Alpes-Côte d\'Azur'
-	'Le Mont-Saint-Michel': 'Normandie'
-	'Grenoble': 'Auvergne-Rhônes-Alpes'
-	'Bordeaux': 'Nouvelle-Aquitaine'
-	'Strasbourg': 'Grand Est'
-	'Perpignan': 'Occitanie'
-	'Saint-Malo': 'Bretagne'
+villes_et_regions = {
+                   'Lyon': 'Auvergne-Rhônes-Alpes',
+                   'Paris': 'Île-de-France',
+                   'Caen': 'Normandie',
+                   'Marseille': 'Provence-Alpes-Côte d\'Azur',
+                   'Le Mont-Saint-Michel': 'Normandie',
+                   'Grenoble': 'Auvergne-Rhônes-Alpes',
+                   'Bordeaux': 'Nouvelle-Aquitaine',
+                   'Strasbourg': 'Grand Est',
+                   'Perpignan': 'Occitanie',
+                   'Saint-Malo': 'Bretagne',
 }
